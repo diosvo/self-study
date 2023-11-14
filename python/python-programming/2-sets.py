89db0662b49031313df4526f4a9f71f7c8d6350c
Dios Vo <vtmn1212@gmail.com>
2023-11-14 21:57:44 +0700

    feat(python): Dictionary with Sets

--- python/python-programming/2-sets.py
+++ python/python-programming/2-sets.py
@@ -11,12 +11,71 @@ animals = ['dog', 'cat', 'bird', 'horse', 'horse', 'cat']
 
 """
 11 - Counter (from collections)
+12 - Basic Collections Review
 """
-print('------------------ Lesson 11 ------------------')
+print('------------------ Lesson 11 -> 12 ------------------')
 
 print(Counter(animals))
 
 """
-12 - Basic Collections Review
+DICTIONARIES w/ Set operations
+
+60 - Working with Sets
 """
-print('------------------ Lesson 12 ------------------')
+print('------------------ Lesson 60 ------------------')
+
+conjunctions = {'for', 'to', 'so', 'by'}
+seen = set()
+
+original_poem = """
+I still feel your touch in my dreams
+Forgive me my weakness, but I don't know why
+Without you it's hard to survive
+'Cause every time we touch, I get this feeling
+And every time we kiss I swear I could fly
+Can't you feel my heart beat fast, I want this to last
+Need you by my side"""
+
+data = original_poem.split()
+
+for word in data:
+    if str.lower(word) in conjunctions:
+        seen.add(str.lower(word))
+
+print(seen)
+
+"""
+61 - Remove duplicates in a List using Sets
+"""
+print('------------------ Lesson 61 ------------------')
+
+colors = ['red', 'red', 'blue', 'blue', 'green', 'blue']
+
+count = [[colors.count(key), key] for key in set(colors)]
+
+print(count)
+
+"""
+62 - Union and Intersection
+63 - Difference and Symmetric Difference
+
+# Union
+>>> my_fav | her_fav
+
+# Intersection
+>>> my_fav & her_fav
+
+63 - Difference and Symmetric Difference
+
+# Difference
+>>> my_fav - her_fav
+
+# Symmetric Difference
+>>> my_fav ^ her_fav
+"""
+print('------------------ Lesson 62 -> 63 ------------------')
+
+my_fav = {'red', 'green', 'blue', 'black', 'purple'}
+her_fav = {'blue', 'purple', 'green'}
+
+print(my_fav ^ her_fav)
